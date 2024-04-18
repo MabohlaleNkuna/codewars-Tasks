42ee8c82e222d96af51ea1429f2f77bdab7cf3b8
wisenkuna <111710433+MabohlaleNkuna@users.noreply.github.com>
2024-04-18 11:33:38 +0200

    Update 5kyu.py

--- 5kyu.py
+++ 5kyu.py
@@ -1,1 +1,14 @@
+"""
+1. Moving Zeros To The End
 
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+"""
+
+#Solution
+
+def move_zeros(lst):
+    zero_list = list(filter(lambda a: a == 0 and not isinstance(a, bool), lst))
+    array = list(filter(lambda a: a != 0 or isinstance(a, bool), lst)) + zero_list
+    return array
