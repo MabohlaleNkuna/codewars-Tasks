f1bdda3c38b33998ef0953d012acb4d4efef04c4
wisenkuna <111710433+MabohlaleNkuna@users.noreply.github.com>
2024-04-19 03:29:45 +0200

    Update 5kyu.py

--- 5kyu.py
+++ 5kyu.py
@@ -9,9 +9,7 @@ move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
 #Solution
 
 def move_zeros(lst):
-    zero_list = list(filter(lambda a: a == 0 and not isinstance(a, bool), lst))
-    array = list(filter(lambda a: a != 0 or isinstance(a, bool), lst)) + zero_list
-    return array
+    return [x for x in lst if x != 0] + [0] * lst.count(0)
 
 
 """
